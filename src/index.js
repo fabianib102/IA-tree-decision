1295d434c5daa94eb3d3d09e8b08acfa1466d441
fabian.ibanez <fabian.ibanez@southworks.com>
2021-09-27 21:44:57 -0300

    add stlyes and decision tree

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-const {app, BrowserWindow} = require('electron');
-const url = require('url');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const url = require("url");
+const path = require("path");
+
+require("electron-reload")(__dirname, {
+
+});
 
 let mainWindow;
 
-app.on('ready', ()=>{
-    mainWindow = new BrowserWindow({});
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'views/index.html'),
-        protocol: 'file',
-        slashes: true
-    }));
+app.on("ready", () => {
+  mainWindow = new BrowserWindow();
+  mainWindow.maximize();
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "views/index.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
 });
-
